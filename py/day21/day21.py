01b1be448106635903c7a3c324366cb2ed0edd91
Stephen McCoy <stephen.mccoy@bjss.com>
2025-02-14 18:34:49 +0000

    Day 21 part 1 done, part 2 WIP

--- py/day21/day21.py
+++ py/day21/day21.py
@@ -9,9 +9,28 @@ test_file = 'test21.txt'
 input_file = 'input21.txt'
 
 # Global variables.
+num_keypad = {'7':(0,0),'8':(1,0),'9':(2,0),
+			  '4':(0,1),'5':(1,1),'6':(2,1),
+			  '1':(0,2),'2':(1,2),'3':(2,2),
+		  	  ' ':(0,3),'0':(1,3),'A':(2,3)
+}
 
-# Infinity distance.
-max_distance = 70 * 70
+# Lookup for what to do to get from one numeric key to another, by using directional keypad.
+num_keypad_transits = []
+
+# Directional keyboard sequences.
+dir_keypad_transits = [
+	('^', 'A', '>'), ('^', '<', 'v<'), ('^', 'v', 'v'), ('^', '>', 'v>'), # ('^', '>', '>v'),
+	('A', '^', '<'), ('A', '<', 'v<<'), # ('A', '<', '<v<'), 
+	('A', 'v', 'v<'), # ('A', 'v', '<v'), 
+	('A', '>', 'v'),
+	('<', 'A', '>>^'), # ('<', 'A', '>^>'), 
+	('<', '^', '>^'), ('<', 'v', '>'), ('<', '>', '>>'),
+	('v', 'A', '>^'), # ('v', 'A', '^>'), 
+	('v', '^', '^'), ('v', '<', '<'), ('v', '>', '>'),
+	('>', 'A', '^'), # ('>', '^', '^<'), 
+	('>', '^', '<^'), ('>', '<', '<<'), ('>', 'v', '<')
+]
 
 # Read the contents of the given file.
 def read_input(file_name):
@@ -19,177 +38,229 @@ def read_input(file_name):
 		return [line.strip() for line in f if len(line) > 0]
 	return False
 
-# Set up the maze, start and goal positions, from input lines.
-def process_lines(lines):
-	global max_distance
-	start = None
-	goal = None
-	num_y = len(lines)
-	num_x = len(lines[0])
-	max_distance = num_x * num_y * 1000
-	grid = np.zeros((num_x, num_y), np.int8)
-	for y in range(num_y):
-		line = lines[y]
-		for x in range(num_x):
-			ch = line[x]
-			if ch == 'S':
-				start = (x,y,'e')
-				ch = '.'
-			elif ch == 'E':
-				goal = (x,y,'e')
-				ch = '.'
-			grid[x,y] = ord(ch)
-	return (grid, (num_x, num_y), start, goal)
+def compile_num_keypad_transits():
+	global num_keypad
+	global num_keypad_transits
 
-# Possible moves and costs: (fwd,1),(left,1000),(right,1000)
-def moves(state, grid):
-	cdot = ord('.')
-	headings = ['n','e','s','w']
-	deltas = {'n':(0,-1), 'e':(1,0), 's':(0,1), 'w':(-1,0)}
+	# Transits should be in min number of straight lines
+	# Transits must avoid (0,3) = ' '
+	# Preference order for end of sub-sequence: >, ^, v, <
 
 	result = []
-	(x,y,h) = state
-	poss_directions = {}
-	for hdg in headings:
-		(dx,dy) = deltas[hdg]
-		if grid[x + dx, y + dy] == cdot:
-			poss_directions[hdg] = (x + dx, y + dy, hdg)
-	
-	if h in poss_directions:
-		result.append(('fwd', 1, poss_directions[h]))
+	numkeys = [k for k in num_keypad if k != ' ']
+	(ax,ay) = num_keypad[' ']		# Avoid!
+	for k1 in numkeys:
+		(x1,y1) = num_keypad[k1]
+		for k2 in numkeys:
+			if k1 == k2:
+				continue
+			(x2,y2) = num_keypad[k2]
+			(x,y) = (x1,y1)
+			kl = []
+			# Avoid the blank space.
+			if y == ay and x2 == ax:
+				while y != y2:
+					y -= 1
+					kl.append('^')
+			elif x == ax and y2 == ay:
+				while x != x2:
+					x += 1
+					kl.append('>')
+			# Which way to go first now?
+			opx = ('>' if x < x2 else '<' if x > x2 else '')
+			opy = ('^' if y > y2 else 'v' if y < y2 else '')
+			if opx != '' and opy != '':
+				if opx == '<':
+					# '<' should go first if needed.
+					while x != x2:
+						x -= 1
+						kl.append('<')
+				else:
+					# Otherwise, do '^' or 'v' before '>' (to finish on '>' button)
+					while y != y2:
+						y += (1 if y < y2 else -1)
+						kl.append(opy)
 
-	for i in range(4):
-		if headings[i] == h:
-			j = i - 1 if i > 0 else 3
-			k = i + 1 if i < 3 else 0
-			if headings[j] in poss_directions:
-				result.append(('left', 1000, (x,y,headings[j])))
-			if headings[k] in poss_directions:
-				result.append(('right', 1000, (x,y,headings[k])))
-			break
-	
-	return result
+			# Correct remaining y error, then x now...
+			while y != y2:
+				dy = 1 if y < y2 else -1
+				y += dy
+				kl.append('^' if dy < 0 else 'v')
 
-def valid_coord(value, max_value):
-	return value >= 0 and value < max_value
+			while x != x2:
+				dx = 1 if x < x2 else -1
+				x += dx
+				kl.append('<' if dx < 0 else '>')
 
-# Find neighbouring points in the grid that are within bounds and empty (accessible).
-def find_neighbours(pos, grid):
-	return moves(pos, grid)
+			ks = ''.join(kl)
+			result.append((k1, k2, ks))
+			
+	num_keypad_transits = result
 
-# Lookup where the node appears in the queue structure.
-def node_in_queue(node, queue):
-	for k,v in queue.items():
-		if node in v:
-			return (node, k)
-	return False
+def lookup_num_transit(from_key, to_key):
+	global num_keypad_transits
+	for (f1, t1, ks) in num_keypad_transits:
+		if f1 == from_key and t1 == to_key:
+			return ks
+	return None
 
-# Unroll the paths from the given map of previous nodes.
-def paths_from_prev(prev, target):
-	paths = []
-	if target in prev and len(prev[target]) > 0:
-		for u in prev[target]:
-			for path in paths_from_prev(prev, u):
-				path.append(target)
-				paths.append(path)
-	else:
-		paths.append([target])
-	return paths
+def lookup_dir_transit(from_key, to_key):
+	global dir_keypad_transits
+	for (f1, t1, ks) in dir_keypad_transits:
+		if f1 == from_key and t1 == to_key:
+			return ks
+	return None
 
-def is_node_goal(node, goal):
-	(xn,yn,_) = node
-	(xg,yg,_) = goal
-	return xn == xg and yn == yg
+# directional keystrokes needed to produce required door code.
+# Assume initially at 'A' key.
+def robot1(code):
+	pos = 'A'
+	seq = []
+	for dest in code:
+		if pos != dest:
+			s = lookup_num_transit(pos, dest)
+			seq.append(s)
+		seq.append('A')
+		pos = dest
+	return seq
 
-# Find ALL Shortest paths using Dijkstra's algorithm.
-def shortest_paths(start, finish, dim, grid):
-	global max_distance
-	# Queue to be sorted (ascending) by distance from start node.
-	(num_x,num_y) = dim
-	# Populate initial distances list.
-	all_nodes = []
-	prev = dict()
-	for x in range(num_x):
-		for y in range(num_y):
-			if grid[x,y] == ord('.'):
-				for h in ['n','e','s','w']:
-					all_nodes.append((x,y,h))
-					# prev[(x,y,h)] = None
+def robot2(code):
+	# Sequence to control robot 1.
+	r1sequence = robot1(code)
+	r1string = ''.join(r1sequence)
+	# Robot 2 sequence.
+	pos = 'A' 
+	seq = []
+	for dest in r1string:
+		if pos != dest:
+			s = lookup_dir_transit(pos, dest)
+			seq.append(s)
+		seq.append('A')
+		pos = dest
+	return seq
 
-	all_nodes.remove(start)
-	# Set up priority order queue.
-	q = dict()
-	q[max_distance] = all_nodes
-	q[0] = [start]
-	# Look at paths from start of increasing length.
-	g = 0
-	shortest_path_cost = 0
-	while len(q) > 0:
-		# Get minimum distance node out of q.
-		while g not in q:
-			g += 1
-		# If we reach infinity there's no path there.
-		if g >= max_distance:
-			break
-		# Quit if all shortest paths found.
-		if shortest_path_cost and g > shortest_path_cost:
-			paths = []
-			for h in ['n','e','s','w']:
-				u = (finish[0], finish[1], h)
-				paths1 = [p for p in paths_from_prev(prev, u) if p[0] == start]
-				paths = paths + paths1
-			return (None,None) if len(paths) == 0 else (paths,shortest_path_cost)
-		# Remove shortest path node.
-		nodes = q[g]
-		if len(nodes) == 0:
-			del q[g]
-			g += 1
-			continue
-		u = nodes.pop()
-		# Update the queue.
-		if len(nodes) > 0:
-			q[g] = nodes
+def robot3(code):
+	# Sequence to control robot 2.
+	r2sequence = robot2(code)
+	r2string = ''.join(r2sequence)
+	# Robot 3 sequence.
+	pos = 'A'
+	seq = []
+	for dest in r2string:
+		if pos != dest:
+			s = lookup_dir_transit(pos, dest)
+			seq.append(s)
+		seq.append('A')
+		pos = dest
+	return seq
+
+# Directional keypad input string to make a robot perform
+# the string of operations given in output string.
+def robot_control_string(output):
+	pos = 'A'
+	seq = []
+	for dest in output:
+		if pos != dest:
+			s = lookup_dir_transit(pos, dest)
+			seq.append(s)
+		seq.append('A')
+		pos = dest
+	return ''.join(seq)
+
+def plan_keys(plan):
+	return sum([plan[k] * len(k) for k in plan])
+
+def robotN(code, n_robots):
+	# Numeric keypad actions.
+	r1sequence = robot1(code)
+	actions = ''.join(r1sequence)
+	print(f"Final robot input is {len(actions)} chars.\n'{actions}'")
+	recipes = {'A':'A'}
+
+	# Create plan for these actions.
+	blocks = [step + 'A' for step in actions[:-1].split('A')]
+	plan1 = {}
+	plan2 = {}
+
+	for b in blocks:
+		if b in plan1:
+			plan1[b] += 1
 		else:
-			del q[g]
-		# Detect goal node.
-		if is_node_goal(u, finish):
-			shortest_path_cost = g
-			continue
-		# Explore from u to neighbouring nodes.
-		neighbours = find_neighbours(u, grid)
-		for (m, c, v) in neighbours:
-			# Make sure v is still in q:
-			in_queue = node_in_queue(v, q)
-			if in_queue:
-				alt = g + c
-				d = in_queue[1]		# Previously recorded distance.
-				if alt <= d:
-					q[d].remove(v)
-					if alt in q:
-						q[alt].append(v)
-					else:
-						q[alt] = [v]
-					if v not in prev:
-						prev[v] = [u]
-					else:
-						prev[v].append(u)
-	return (None,None)
+			plan1[b] = 1
+
+	for ri in range(n_robots):
+		# Break out parts of plan so far.
+		for a in plan2:
+			acount = plan2[a]
+			for br in a[:-1].split('A'):
+				bf = br + 'A'
+				if bf in plan1:
+					plan1[bf] += acount
+				else:
+					plan1[bf] = acount
+
+		plan2 = {}
+		for b1 in plan1:
+			if b1 in recipes:
+				b2 = recipes[b1]
+			else:
+				b2 = robot_control_string(b1)
+				recipes[b1] = b2
+
+			if b2 in plan2:
+				plan2[b2] += plan1[b1]
+			else:
+				plan2[b2] = plan1[b1]
+		
+		plan1 = {}
+
+	return plan2
+
+def control_string(code):
+	seq = robot3(code)
+	sls = ''.join(seq)
+	return sls
+
+def complexity(code, control_string):
+	numpart = int(code[:-1])
+	numchars = len(control_string)
+	return numchars * numpart
 
 def part1_for(file_name):
 	lines = read_input(file_name)
-	(grid, (num_x, num_y), start, goal) = process_lines(lines)
-	(paths,cost) = shortest_paths(start, goal, (num_x, num_y), grid)
-	return cost
+	compile_num_keypad_transits()
+	score = 0
+	for line in lines:
+		code = line.strip()
+		c_string = control_string(code)
+		c_score = complexity(code, c_string)
+		print(f'Code {code} has complexity {c_score} for {len(c_string)} buttons {c_string}')
+		score += c_score
+	return score
+
+def plan_complexity(code, plan):
+	numpart = int(code[:-1])
+	nc = 0
+	for b in plan:
+		nc += plan[b] * len(b)
+	return nc * numpart
 
 def part2_for(file_name):
 	lines = read_input(file_name)
-	(grid, (num_x, num_y), start, goal) = process_lines(lines)
-	(paths,cost) = shortest_paths(start, goal, (num_x, num_y), grid)
-	nodes = set()
-	for p in paths:
-		for (x,y,h) in p:
-			nodes.add((x,y))
-	return len(nodes)
+	compile_num_keypad_transits()
+	score = 0
+	for line in lines:
+		code = line.strip()
+		plan = robotN(code, 25)
+		c_score = plan_complexity(code, plan)
+		print(f'Code {code} has complexity {c_score}')
+		score += c_score
+	return score
+
+# 159907381897160 too low (24 rbt)
+# 314444784819888 too high (25 rbt)
+# 410660523597582 too high (25 rbt)
+
 
 # Main processing.
 print('Advent of Code 2024 - Day 21, Part 1.')
@@ -202,10 +273,6 @@ count = part1_for(input_file)
 print(f"Result is {count}")
 
 print('Part 2.')
-print('Running test...')
-count = part2_for(test_file)
-print(f"Result is {count}")
-
 print('Running full input...')
 count = part2_for(input_file)
 print(f"Result is {count}")
