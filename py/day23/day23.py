96d9b5d568b9e21f75c4fa848282a357d4f800e3
Stephen McCoy <stephen.mccoy@bjss.com>
2025-02-18 14:42:41 +0000

    Day 23 done

--- py/day23/day23.py
+++ py/day23/day23.py
@@ -2,16 +2,9 @@
 # Advent of Code 2024, Day 23 - LAN Party
 #
 
-import numpy as np
-
 # Files.
 test_file = 'test23.txt'
 input_file = 'input23.txt'
-
-# Global variables.
-
-# Infinity distance.
-max_distance = 70 * 70
 
 # Read the contents of the given file.
 def read_input(file_name):
@@ -19,177 +12,87 @@ def read_input(file_name):
 		return [line.strip() for line in f if len(line) > 0]
 	return False
 
-# Set up the maze, start and goal positions, from input lines.
 def process_lines(lines):
-	global max_distance
-	start = None
-	goal = None
-	num_y = len(lines)
-	num_x = len(lines[0])
-	max_distance = num_x * num_y * 1000
-	grid = np.zeros((num_x, num_y), np.int8)
-	for y in range(num_y):
-		line = lines[y]
-		for x in range(num_x):
-			ch = line[x]
-			if ch == 'S':
-				start = (x,y,'e')
-				ch = '.'
-			elif ch == 'E':
-				goal = (x,y,'e')
-				ch = '.'
-			grid[x,y] = ord(ch)
-	return (grid, (num_x, num_y), start, goal)
-
-# Possible moves and costs: (fwd,1),(left,1000),(right,1000)
-def moves(state, grid):
-	cdot = ord('.')
-	headings = ['n','e','s','w']
-	deltas = {'n':(0,-1), 'e':(1,0), 's':(0,1), 'w':(-1,0)}
-
-	result = []
-	(x,y,h) = state
-	poss_directions = {}
-	for hdg in headings:
-		(dx,dy) = deltas[hdg]
-		if grid[x + dx, y + dy] == cdot:
-			poss_directions[hdg] = (x + dx, y + dy, hdg)
-	
-	if h in poss_directions:
-		result.append(('fwd', 1, poss_directions[h]))
-
-	for i in range(4):
-		if headings[i] == h:
-			j = i - 1 if i > 0 else 3
-			k = i + 1 if i < 3 else 0
-			if headings[j] in poss_directions:
-				result.append(('left', 1000, (x,y,headings[j])))
-			if headings[k] in poss_directions:
-				result.append(('right', 1000, (x,y,headings[k])))
-			break
-	
-	return result
-
-def valid_coord(value, max_value):
-	return value >= 0 and value < max_value
-
-# Find neighbouring points in the grid that are within bounds and empty (accessible).
-def find_neighbours(pos, grid):
-	return moves(pos, grid)
-
-# Lookup where the node appears in the queue structure.
-def node_in_queue(node, queue):
-	for k,v in queue.items():
-		if node in v:
-			return (node, k)
-	return False
-
-# Unroll the paths from the given map of previous nodes.
-def paths_from_prev(prev, target):
-	paths = []
-	if target in prev and len(prev[target]) > 0:
-		for u in prev[target]:
-			for path in paths_from_prev(prev, u):
-				path.append(target)
-				paths.append(path)
-	else:
-		paths.append([target])
-	return paths
-
-def is_node_goal(node, goal):
-	(xn,yn,_) = node
-	(xg,yg,_) = goal
-	return xn == xg and yn == yg
-
-# Find ALL Shortest paths using Dijkstra's algorithm.
-def shortest_paths(start, finish, dim, grid):
-	global max_distance
-	# Queue to be sorted (ascending) by distance from start node.
-	(num_x,num_y) = dim
-	# Populate initial distances list.
-	all_nodes = []
-	prev = dict()
-	for x in range(num_x):
-		for y in range(num_y):
-			if grid[x,y] == ord('.'):
-				for h in ['n','e','s','w']:
-					all_nodes.append((x,y,h))
-					# prev[(x,y,h)] = None
-
-	all_nodes.remove(start)
-	# Set up priority order queue.
-	q = dict()
-	q[max_distance] = all_nodes
-	q[0] = [start]
-	# Look at paths from start of increasing length.
-	g = 0
-	shortest_path_cost = 0
-	while len(q) > 0:
-		# Get minimum distance node out of q.
-		while g not in q:
-			g += 1
-		# If we reach infinity there's no path there.
-		if g >= max_distance:
-			break
-		# Quit if all shortest paths found.
-		if shortest_path_cost and g > shortest_path_cost:
-			paths = []
-			for h in ['n','e','s','w']:
-				u = (finish[0], finish[1], h)
-				paths1 = [p for p in paths_from_prev(prev, u) if p[0] == start]
-				paths = paths + paths1
-			return (None,None) if len(paths) == 0 else (paths,shortest_path_cost)
-		# Remove shortest path node.
-		nodes = q[g]
-		if len(nodes) == 0:
-			del q[g]
-			g += 1
-			continue
-		u = nodes.pop()
-		# Update the queue.
-		if len(nodes) > 0:
-			q[g] = nodes
+	nodes = dict()
+	links = set()
+	for line in lines:
+		t = line.split('-')
+		lhs = t[0]
+		rhs = t[1]
+		if lhs in nodes:
+			nodes[lhs] += 1
 		else:
-			del q[g]
-		# Detect goal node.
-		if is_node_goal(u, finish):
-			shortest_path_cost = g
-			continue
-		# Explore from u to neighbouring nodes.
-		neighbours = find_neighbours(u, grid)
-		for (m, c, v) in neighbours:
-			# Make sure v is still in q:
-			in_queue = node_in_queue(v, q)
-			if in_queue:
-				alt = g + c
-				d = in_queue[1]		# Previously recorded distance.
-				if alt <= d:
-					q[d].remove(v)
-					if alt in q:
-						q[alt].append(v)
-					else:
-						q[alt] = [v]
-					if v not in prev:
-						prev[v] = [u]
-					else:
-						prev[v].append(u)
-	return (None,None)
+			nodes[lhs] = 1
+		if rhs in nodes:
+			nodes[rhs] += 1
+		else:
+			nodes[rhs] = 1
+		links.add((lhs,rhs))
+	return nodes,links
+
+def connected(n1, n2, links):
+	return (n1,n2) in links or (n2,n1) in links
+
+def counted(n1, n2, n3):
+	return n1.startswith('t') or n2.startswith('t') or n3.startswith('t')
+
+def find_triples(nodes, links, need_ts):
+	cnodes = [n for n in nodes if nodes[n] > 1]
+	triples = []
+	for n1 in cnodes:
+		for n2 in cnodes:
+			if connected(n1, n2, links):
+				for n3 in cnodes:
+					if connected(n1, n3, links) and connected(n2, n3, links):
+						if not need_ts or counted(n1,n2,n3):
+							snl = sorted([n1,n2,n3])
+							if snl not in triples:
+								triples.append(snl)
+	return triples
+
+def extend_groups(groups, nodes, links):
+	done = False
+	while not done:
+		ngroups = []
+		done = True
+		for g in groups:
+			for n1 in nodes:
+				if n1 in g:
+					continue
+				if all(connected(n2, n1, links) for n2 in g):
+					g2 = sorted([n1] + g)
+					if g2 not in ngroups:
+						ngroups.append(g2)
+						done = False
+		if len(ngroups) > 0:
+			groups = ngroups
+	return groups
+
+def largest_group(groups):
+	gs = 0
+	gi = 0
+	for i in range(len(groups)):
+		if len(groups[i]) > gs:
+			gi = i
+			gs = len(groups[i])
+	return groups[gi]
+
+def group_password(group):
+	return ','.join(sorted(group))
 
 def part1_for(file_name):
 	lines = read_input(file_name)
-	(grid, (num_x, num_y), start, goal) = process_lines(lines)
-	(paths,cost) = shortest_paths(start, goal, (num_x, num_y), grid)
-	return cost
+	nodes, links = process_lines(lines)
+	triples = find_triples(nodes, links, True)
+	return len(triples)
 
 def part2_for(file_name):
 	lines = read_input(file_name)
-	(grid, (num_x, num_y), start, goal) = process_lines(lines)
-	(paths,cost) = shortest_paths(start, goal, (num_x, num_y), grid)
-	nodes = set()
-	for p in paths:
-		for (x,y,h) in p:
-			nodes.add((x,y))
-	return len(nodes)
+	nodes, links = process_lines(lines)
+	triples = find_triples(nodes, links, False)
+	groups = extend_groups(triples, nodes, links)
+	big_group = largest_group(groups)
+	return group_password(big_group)
 
 # Main processing.
 print('Advent of Code 2024 - Day 23, Part 1.')
